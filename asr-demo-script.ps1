5663b285cbd42c6511a811a25b571a61ee5cc101
javieriroman <j.roman.upr@gmail.com>
2018-03-09 14:46:06 -0600

    add it powershell to download&import the post script from the cloud shell

--- asr-demo-script.ps1
+++ asr-demo-script.ps1
@@ -22,10 +22,17 @@ New-AzureRmAutomationVariable -ResourceGroupName asr-target -AutomationAccountNa
 
 New-AzureRmAutomationVariable -ResourceGroupName asr-target -AutomationAccountName asr-automation -Name myrecoveryplan-TM -Value asrdemoarrow -Encrypted $false
 New-AzureRmAutomationVariable -ResourceGroupName asr-target -AutomationAccountName asr-automation -Name myrecoveryplan-TMRG -Value asr-source -Encrypted $false 
+#> Now you have to import a post script to be execute at the end of the recovery plan to glue everything together.
+#> Here you have 2 options, powershell from Cloud Shell or portal.
+#>Below the powershell option from your Cloud Shell
 
-#> Then go the link below to download the post script i create in order to import it as runbook.
+Start-BitsTransfer -source https://raw.githubusercontent.com/javieriroman/azurepowershell/master/asr-demo-postscript.ps1 -Destination $HOME/postscript.ps1
+Import-AzureRmAutomationRunbook -AutomationAccountName asr-automation -Name postscript -ResourceGroupName asr-target -Type PowerShell -Path $HOME/postscript.ps1 -Published
+
+#> Below the instruction for the portal option
+#> Then go the link below to download the post script here created in order to import it as runbook.
 #> https://raw.githubusercontent.com/javieriroman/azurepowershell/master/asr-demo-postscript.ps1
-
 #> Go to the automation account, select runbook, select create new, put name and type "powershell"
 #> Once create select the created runbook and select edit, copy the content from the link above, 
-#> then select save and publish. Now you can use it on the recovery plan.  
+#> then select save and publish. Another option is to import the script from the portal.
+#> Now you can use it on the recovery plan.  
